6f508f81c4950b0575db95c3d72d63fdff5e57f0
Sreya-Vallabhaneni <114380178+Sreya-Vallabhaneni@users.noreply.github.com>
2023-01-28 22:21:32 -0500

    Add files via upload

---
+++ ThreeSumBenchmark.java
@@ -0,0 +1,161 @@
+package edu.neu.coe.info6205.threesum;
+
+import edu.neu.coe.info6205.util.Benchmark_Timer;
+import edu.neu.coe.info6205.util.TimeLogger;
+import edu.neu.coe.info6205.util.Utilities;
+import edu.neu.coe.info6205.util.Stopwatch;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+public class ThreeSumBenchmark {
+    public ThreeSumBenchmark(int runs, int n, int m) {
+        this.runs = runs;
+        this.supplier = new Source(n, m).intsSupplier(10);
+        this.n = n;
+    }
+
+    public void runBenchmarks() {
+        System.out.println("ThreeSumBenchmark: N=" + n);
+        benchmarkThreeSum("ThreeSumQuadratic", (xs) -> new ThreeSumQuadratic(xs).getTriples(), n, timeLoggersQuadratic);
+        benchmarkThreeSum("ThreeSumQuadrithmic", (xs) -> new ThreeSumQuadrithmic(xs).getTriples(), n, timeLoggersQuadrithmic);
+        benchmarkThreeSum("ThreeSumCubic", (xs) -> new ThreeSumCubic(xs).getTriples(), n, timeLoggersCubic);
+        benchmarkThreeSum("ThreeSumQuadraticWithCalipers", (xs) -> new ThreeSumQuadraticWithCalipers(xs).getTriples(), n, timeLoggersQuadraticWithCalipers);
+    }
+
+    public static void main(String[] args) {
+        new ThreeSumBenchmark(100, 250, 250).runBenchmarks();
+        new ThreeSumBenchmark(50, 500, 500).runBenchmarks();
+        new ThreeSumBenchmark(20, 1000, 1000).runBenchmarks();
+        new ThreeSumBenchmark(10, 2000, 2000).runBenchmarks();
+        new ThreeSumBenchmark(5, 4000, 4000).runBenchmarks();
+        new ThreeSumBenchmark(3, 8000, 8000).runBenchmarks();
+        new ThreeSumBenchmark(2, 16000, 16000).runBenchmarks();
+    }
+
+    private void benchmarkThreeSum(final String description, final Consumer<int[]> function, int n, final TimeLogger[] timeLoggers) {
+        if (description.equals("ThreeSumCubic") && n >4000) return;
+
+        if(description.equals("ThreeSumCubic") && n<=4000)
+        {
+            double Runtime = 0;
+            int k =runs;
+            while(k-->0)
+            {
+                try
+                {
+                    Stopwatch timer = new Stopwatch();
+                    function.accept(supplier.get());
+                    long timing = timer.lap();
+                    Runtime += timing/(double) runs;
+                }
+                catch(Exception e)
+                {
+                    System.out.println("Exception Caught"+e);
+                }
+            }
+            System.out.println(description);
+            for(TimeLogger timeLogger:timeLoggers)
+            {
+                timeLogger.log(Runtime,n);
+            }
+        }
+
+        if(description.equals("ThreeSumQuadrithmic"))
+        {
+            double Runtime = 0;
+            int k = runs;
+            while(k-->0)
+            {
+                try
+                {
+                    Stopwatch timer = new Stopwatch();
+                    function.accept(supplier.get());
+                    long timing = timer.lap();
+                    Runtime += timing/(double) runs;
+                }
+                catch(Exception e)
+                {
+                    System.out.println("Exception Caught"+e);
+                }
+            }
+            System.out.println(description);
+            for(TimeLogger timeLogger:timeLoggers)
+            {
+                timeLogger.log(Runtime,n);
+            }
+        }
+
+        if(description.equals("ThreeSumQuadratic"))
+        {
+            double Runtime = 0;
+            int k = runs;
+            while(k-->0)
+            {
+                try
+                {
+                    Stopwatch timer = new Stopwatch();
+                    function.accept(supplier.get());
+                    long timing = timer.lap();
+                    Runtime += timing/(double) runs;
+                }
+                catch(Exception e)
+                {
+                    System.out.println("Exception Caught"+e);
+                }
+            }
+            System.out.println(description);
+            for(TimeLogger timeLogger:timeLoggers)
+            {
+                timeLogger.log(Runtime,n);
+            }
+        }
+
+        if(description.equals("ThreeSumQuadraticWithCalipers"))
+        {
+            double Runtime = 0;
+            int k=runs;
+            while(k-->0)
+            {
+                try
+                {
+                    Stopwatch timer = new Stopwatch();
+                    function.accept(supplier.get());
+                    long timing = timer.lap();
+                    Runtime += timing/(double) runs;
+                }
+                catch(Exception e)
+                {
+                    System.out.println("Exception Caught"+e);
+                }
+            }
+            System.out.println(description);
+            for(TimeLogger timeLogger:timeLoggers)
+            {
+                timeLogger.log(Runtime,n);
+            }
+        }
+    }
+
+    private final static TimeLogger[] timeLoggersCubic = {
+            new TimeLogger("Raw time per run (mSec): ", (time, n) -> time),
+            new TimeLogger("Normalized time per run (n^3): ", (time, n) -> time / n / n / n * 1e6)
+    };
+    private final static TimeLogger[] timeLoggersQuadrithmic = {
+            new TimeLogger("Raw time per run (mSec): ", (time, n) -> time),
+            new TimeLogger("Normalized time per run (n^2 log n): ", (time, n) -> time / n / n / Utilities.lg(n) * 1e6)
+    };
+    private final static TimeLogger[] timeLoggersQuadratic = {
+            new TimeLogger("Raw time per run (mSec): ", (time, n) -> time),
+            new TimeLogger("Normalized time per run (n^2): ", (time, n) -> time / n / n * 1e6)
+    };
+
+    private final static TimeLogger[] timeLoggersQuadraticWithCalipers = {
+        new TimeLogger("Raw time per run (mSec): ", (time, n) -> time),
+        new TimeLogger("Normalized time per run (n^2): ", (time, n) -> time / n / n * 1e6)
+};
+
+    private final int runs;
+    private final Supplier<int[]> supplier;
+    private final int n;
+}
